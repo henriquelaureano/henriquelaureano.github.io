5ea4246bfd8262c95a43e041bf4110d0728641b6
Henrique Laureano <henriquelaureano@outlook.com>
2020-12-30 11:06:24 -0300

    daily backup

---
+++ courses/emr15/piecewise/js/anima.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#anima').scianimator({
+	    'images': ['anima/anima1.png', 'anima/anima2.png', 'anima/anima3.png', 'anima/anima4.png', 'anima/anima5.png', 'anima/anima6.png', 'anima/anima7.png', 'anima/anima8.png', 'anima/anima9.png', 'anima/anima10.png', 'anima/anima11.png', 'anima/anima12.png', 'anima/anima13.png', 'anima/anima14.png', 'anima/anima15.png', 'anima/anima16.png', 'anima/anima17.png', 'anima/anima18.png', 'anima/anima19.png', 'anima/anima20.png', 'anima/anima21.png', 'anima/anima22.png', 'anima/anima23.png', 'anima/anima24.png', 'anima/anima25.png', 'anima/anima26.png', 'anima/anima27.png', 'anima/anima28.png', 'anima/anima29.png', 'anima/anima30.png', 'anima/anima31.png', 'anima/anima32.png', 'anima/anima33.png', 'anima/anima34.png', 'anima/anima35.png', 'anima/anima36.png', 'anima/anima37.png', 'anima/anima38.png', 'anima/anima39.png', 'anima/anima40.png', 'anima/anima41.png'],
+	    'width': 600,
+	    'delay': 100,
+	    'loopMode': 'loop'
+	});
+	$('#anima').scianimator('play');
+    });
+})(jQuery);
