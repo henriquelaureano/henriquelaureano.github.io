0f7990c95ae8170901ad1536b8d61fc09356344c
Henrique Laureano <henriquelaureano@outlook.com>
2021-01-31 00:06:20 -0300

    daily backup

---
+++ THESIS/server/scheike/kkholst-mcif/R/cif_loglik.R
@@ -0,0 +1,85 @@
+cif.loglik <- function(datprep, par, nq, stepsize, iter, grad=FALSE){
+
+##-----------------------------------------------------------------------
+## Parameters
+##-----------------------------------------------------------------------
+b1 <- par[1]
+b2 <- par[2]
+
+g1 <- par[3]
+g2 <- par[4]
+
+a1 <- exp(par[5])
+a2 <- exp(par[6])
+
+## Parameters for variance-covariance
+s1 <- par[7]
+s2 <- par[8]
+s3 <- par[9]
+s4 <- par[10]
+s5 <- par[11]
+
+s6 <- par[12]
+s7 <- par[13]
+s8 <- par[14]
+s9 <- par[15]
+s10 <- par[16]
+
+##-----------------------------------------------------------------------
+## Sigma
+##-----------------------------------------------------------------------
+## Cholesky (upper triangle)
+L.chol <- matrix(c(exp(s1),0,0,0,
+                     s5,exp(s2),0,0,
+                     s6,s8,exp(s3),0,
+                     s7,s9,s10,exp(s4)),
+                   nrow=4, ncol=4)
+
+## Variance-covariance matrix of eta and u
+vcv <- t(L.chol)%*%L.chol
+
+## Sigma for likelihood
+sigma <- SigmaGen(vcv, 2)
+
+##-----------------------------------------------------------------------
+## The rest
+##-----------------------------------------------------------------------
+## Causes
+causes <- datprep$causes
+ncauses <- 2 # depends on data
+
+## Weights
+w <- datprep$weights
+
+## Empical Bayes
+eb0 <- t(datprep$eb0) # Must for some reason (aka my coding [LC]) be transposed...
+
+## Prepping beta
+beta <- cbind(datprep$x.1%*%b1, datprep$x.1%*%b2, datprep$x.2%*%b1, datprep$x.2%*%b2)
+
+## Prepping alpha
+alpha <- cbind(datprep$gt1*a1, datprep$gt1*a2, datprep$gt2*a1, datprep$gt2*a2)
+
+## Prepping dalpha
+dalpha <- cbind(datprep$dgt1*a1, datprep$dgt1*a2, datprep$dgt2*a1, datprep$dgt2*a2)
+
+## Prepping gamma
+gamma <- cbind(datprep$x.1%*%g1, datprep$x.1%*%g2, datprep$x.2%*%g1, datprep$x.2%*%g2)
+
+##-----------------------------------------------------------------------
+## Estimation of loglikelihood
+##-----------------------------------------------------------------------
+ll <- loglik(sigma, ncauses, causes, alpha, dalpha, beta, gamma, eb0, nq, stepsize, iter)
+
+if (grad==FALSE){
+    llw <- w*ll
+    k <- -sum(llw)
+    k
+    return(k)
+}
+if(grad==TRUE){
+    dg <- as.data.frame(cbind(as.numeric(ll),as.numeric(datprep$ID)))
+    pc1 <- melt(tapply(dg$V1, dg$V2, sum))[,2]
+    return(pc1)
+}
+}
---
+++ THESIS/server/scheike/nelder22.R
@@ -0,0 +1,91 @@
+## henrique laureano (.github.io)
+## date: 30-jan-2021
+## scheike's pairwise model via nelder-mead in the structure 22---------
+
+(args <- commandArgs())
+i <- abs(as.numeric(args[7]))
+
+pacman::p_load('Rcpp', 'mets')
+
+source('kkholst-mcif/inst/examples/helpfunctions_test.R')
+sourceCpp('kkholst-mcif/src/loglik.cpp')
+load('../data22.RData')
+
+scheike <- function(theta, t, delta=80, J, causes) {
+    vcv <- matrix(0, nrow=4, ncol=4)
+    rhoZ_12 <- tanh(theta['rho_12'])
+    rhoZ_13 <- tanh(theta['rho_13'])
+    rhoZ_14 <- tanh(theta['rho_14'])
+    rhoZ_23 <- tanh(theta['rho_23'])
+    rhoZ_24 <- tanh(theta['rho_24'])
+    rhoZ_34 <- tanh(theta['rho_34'])
+    vcv[1, ] <- c(
+        exp(theta['s2_1']),
+        rhoZ_12*sqrt(exp(theta['s2_1']))*sqrt(exp(theta['s2_2'])),
+        rhoZ_13*sqrt(exp(theta['s2_1']))*sqrt(exp(theta['s2_3'])),
+        ## rhoZ_14*sqrt(exp(theta['s2_1']))*sqrt(exp(theta['s2_4'])))
+        0)
+    vcv[2, ] <- c(
+        rhoZ_12*sqrt(exp(theta['s2_2']))*sqrt(exp(theta['s2_1'])),
+        exp(theta['s2_2']),
+        ## rhoZ_23*sqrt(exp(theta['s2_2']))*sqrt(exp(theta['s2_3'])),
+        0, 
+        rhoZ_24*sqrt(exp(theta['s2_2']))*sqrt(exp(theta['s2_4'])))
+    vcv[3, ] <- c(
+        rhoZ_13*sqrt(exp(theta['s2_3']))*sqrt(exp(theta['s2_1'])),
+        ## rhoZ_23*sqrt(exp(theta['s2_3']))*sqrt(exp(theta['s2_2'])),
+        0, 
+        exp(theta['s2_3']),
+        rhoZ_34*sqrt(exp(theta['s2_3']))*sqrt(exp(theta['s2_4'])))
+    vcv[4, ] <- c(
+        ## rhoZ_14*sqrt(exp(theta['s2_4']))*sqrt(exp(theta['s2_1'])),
+        0, 
+        rhoZ_24*sqrt(exp(theta['s2_4']))*sqrt(exp(theta['s2_2'])),
+        rhoZ_34*sqrt(exp(theta['s2_4']))*sqrt(exp(theta['s2_3'])),
+        exp(theta['s2_4']))
+    gt1 <- gt2 <- atanh((t-delta/2)/(delta/2))
+    dgt1 <- dgt2 <- 0.5*delta/(t*(delta-t))
+    alpha <- cbind(gt1*theta['a1'], gt1*theta['a2'],
+                   gt2*theta['a1'], gt2*theta['a2'])
+    dalpha <- cbind(dgt1*theta['a1'], dgt1*theta['a2'],
+                    dgt2*theta['a1'], dgt2*theta['a2'])
+    x.1 <- as.matrix(cbind(rep(1, 2*J)))
+    x.2 <- as.matrix(cbind(rep(1, 2*J)))
+    beta <- cbind(x.1%*%theta['b1'], x.1%*%theta['b2'],
+                  x.2%*%theta['b1'], x.2%*%theta['b2'])
+    gamma <- cbind(x.1%*%theta['g1'], x.1%*%theta['g2'],
+                   x.2%*%theta['g1'], x.2%*%theta['g2'])
+    out <- loglik(sigma=SigmaGen(vcv, 2, old=FALSE),
+                  ncauses=2, causes=causes,
+                  alpha=alpha, dalpha=dalpha,
+                  beta=beta, gamma=gamma,
+                  eb0=t(matrix(0, nrow=2*J, ncol=2)), nq=3)
+    return(-sum(out))
+}
+
+i <- 1
+theta <- c(b1=initFixed[i, 1], b2=initFixed[i, 2],
+           g1=initFixed[i, 3], g2=initFixed[i, 4],
+           a1=initFixed[i, 5], a2=initFixed[i, 6],
+           s2_1=log(0.2), s2_2=log(0.3), s2_3=log(0.4), s2_4=log(0.5),
+           rho_12=atanh(0.15/sqrt(0.2*0.3)),
+           rho_13=atanh(0.1/sqrt(0.2*0.4)),
+           rho_24=atanh(0.1/sqrt(0.3*0.5)),
+           rho_34=atanh(0.15/sqrt(0.4*0.5)))
+
+J <- 3e4
+t <- rep(seq(from=30, to=79.5, by=0.5), length.out=2*J)
+
+causes <- y[[i]][, 1:2]
+
+scheike(theta, t, delta=80, J, causes)
+
+where <- 'nelder22'
+
+opt <- try(optim(obj$par, obj$fn, obj$gr), silent=TRUE)
+if (class(opt)!='try-error') {
+    write.table(rbind(c(opt$par, opt$convergence)),
+                file=paste0(where, '.txt'),
+                append=TRUE, col.names=FALSE)
+}
+## END------------------------------------------------------------------
