79a37425ba0726d1f401adcca9a9c20b0538f558
Henrique Laureano <henriquelaureano@outlook.com>
2021-02-23 18:23:37 -0300

    daily backup

--- THESIS/code/functions.R
+++ THESIS/code/functions.R
@@ -1,13 +1,23 @@
 ##----------------------------------------------------------------------
 ##                                                     Henrique Laureano
 ##                                            henriquelaureano.github.io
-##                                      2021-fev-22 · Curitiba/PR/Brazil
+##                                      2021-fev-23 · Curitiba/PR/Brazil
 ##----------------------------------------------------------------------
-
 ## multiGLMM: A MULTINOMIAL GLMM FOR CLUSTERED COMPETING RISKS DATA
 
-sigmaPD <- function(s2_1, s2_2, s2_3, s2_4,
-                    rho12, rho13, rho14, rho23, rho24, rho34)
+sigmaPD2 <- function(s2_1, s2_2, rho12)
+{
+    cov12 <- rho12*sqrt(s2_1)*sqrt(s2_2)
+
+    Sigma <- matrix(c(s2_1, cov12, cov12, s2_2), nrow=2)
+
+    if (is.matrix(chol(Sigma)))
+    
+    return(Sigma)
+}
+
+sigmaPD4 <- function(s2_1, s2_2, s2_3, s2_4,
+                     rho12, rho13, rho14, rho23, rho24, rho34)
 {
     cov12 <- rho12*sqrt(s2_1)*sqrt(s2_2)
     cov13 <- rho13*sqrt(s2_1)*sqrt(s2_3)
@@ -26,7 +36,57 @@ sigmaPD <- function(s2_1, s2_2, s2_3, s2_4,
     return(Sigma)
 }
 
-ps <- function(J, cs, time, delta=80, beta, gama, w, Sigma=NULL)
+ps2 <- function(J, cs, time, delta=80, beta, gama, w,
+                type=c('risk', 'trajectory'), Sigma=NULL)
+{
+    ZU <- matrix(0, nrow=cs*J, ncol=2)
+
+    if (!is.null(Sigma)) {
+
+        if (!is.matrix(Sigma) | dim(Sigma)[1]!=2 | dim(Sigma)[2]!=2)
+            stop('Sigma must be a 2x2 matrix')
+
+        Z <- Matrix::bdiag(replicate(J, rep(1, cs), simplify=FALSE))
+        U <- mvtnorm::rmvnorm(J,
+                              mean=rep(0, dim(Sigma)[1]),
+                              sigma=Sigma)
+        ZU <- Z%*%U
+    }
+    gt <- 0.5*log(time/(delta-time))
+    
+    switch(
+        type,
+        risk=
+            {
+                risk1 <- exp(beta[1] + ZU[, 1])
+                risk2 <- exp(beta[2] + ZU[, 2])
+                
+                x1 <- w[1]*gt - gama[1]
+                x2 <- w[2]*gt - gama[2]
+            },
+        trajectory=
+            {
+                risk1 <- exp(beta[1])
+                risk2 <- exp(beta[2])
+                
+                x1 <- w[1]*gt - gama[1] - ZU[, 1]
+                x2 <- w[2]*gt - gama[2] - ZU[, 2]
+            },
+        stop("Argument ``type`` off the labels")
+    )
+    dgt <- delta/(2*time*(delta-time))
+
+    level <- 1 + risk1 + risk2
+
+    p1 <- risk1/level * w[1]*dgt * dnorm(x1)
+    p2 <- risk2/level * w[2]*dgt * dnorm(x2)
+
+    p3 <- 1 - p1 - p2
+
+    return(cbind(p1, p2, p3))
+}
+
+ps4 <- function(J, cs, time, delta=80, beta, gama, w, Sigma=NULL)
 {
     ZU <- matrix(0, nrow=cs*J, ncol=2)
 
@@ -60,11 +120,20 @@ ps <- function(J, cs, time, delta=80, beta, gama, w, Sigma=NULL)
     return(cbind(p1, p2, p3))
 }
 
-datasimu <- function(J, cs, time, delta=80, beta, gama, w, Sigma=NULL)
+datasimu <- function(J, cs, time, delta=80, beta, gama, w,
+                     latent=c('partial', 'complete'), type=NULL,
+                     Sigma=NULL)
 {
-    ps <- ps(J=J, cs=cs, time=time, delta=delta,
-             beta=beta, gama=gama, w=w, Sigma=Sigma)
-
+    switch(latent,
+           partial={ ps <- ps2(J=J, cs=cs, time=time, delta=delta,
+                               beta=beta, gama=gama, w=w,
+                               type=type, Sigma=Sigma)
+           },
+           complete={ ps <- ps4(J=J, cs=cs, time=time, delta=delta,
+                                beta=beta, gama=gama, w=w, Sigma=Sigma)
+           },
+           stop('Sigma must be a 2x2 or 4x4 matrix'))
+    
     out <- mc2d::rmultinomial(cs*J, 1, prob=ps)
     colnames(out) <- paste0('y', 1:3)
 
@@ -72,17 +141,19 @@ datasimu <- function(J, cs, time, delta=80, beta, gama, w, Sigma=NULL)
 }
 
 future_datasimu <- function(J, cs, n, time, delta=80, beta, gama, w,
+                            latent=c('partial', 'complete'), type=NULL,
                             Sigma=NULL)
 {
     y <- furrr::future_map(
                     rep(J, n),
                     ~datasimu(.x, cs=cs, time=time, delta=delta,
-                              beta=beta, gama=gama, w=w, Sigma=Sigma),
+                              beta=beta, gama=gama, w=w,
+                              latent=latent, type=type, Sigma=Sigma),
                     .options=furrr_options(seed=NULL))
     return(y)
 }
 
-modelZ <- function(J, cs)
+matrixZ <- function(J, cs)
 {
     blocks <- replicate(J, rep(1, cs), simplify=FALSE)
 
@@ -96,66 +167,8 @@ checkDLL <- function(dll)
     if (!dll%in%names(getLoadedDLLs()))
     {
         cat('Loading DLL\n')
-        dyn.load(TMB::dynlib(dll))
+        ## ~/Git/henriquelaureano.github.io/THESIS/code/
+        dyn.load(TMB::dynlib(paste0('cpps/', dll)))
         invisible(TMB::config(tape.parallel=FALSE, DLL=dll))
     }
-}
-
-getguess <- function(dll, y, beta)
-{
-    if (names(beta)[1] != 'beta1' | names(beta)[2] != 'beta2')
-        stop('Check beta names')
-
-    n <- length(y)
-
-    out <- matrix(NA, nrow=n, ncol=length(beta))
-    colnames(out) <- names(beta)
-    
-    for (i in seq(n))
-    {
-        checkDLL(dll)
-        obj <- TMB::MakeADFun(data=list(Y=y[[i]]),
-                              parameters=list(beta1=beta['beta1'],
-                                              beta2=beta['beta2']),
-                              DLL=dll,
-                              hessian=TRUE, silent=TRUE)
-        
-        opt <- nlminb(obj$par, obj$fn, obj$gr)
-
-        out[i, ] <- opt$par
-    }
-    return(out)
-}
-
-GLMMfit <- function(dll, y, Z, pars)
-{
-    checkDLL(dll)
-    obj <- TMB::MakeADFun(data=list(Y=y, Z=Z),
-                          parameters=pars,
-                          DLL=dll,
-                          random='U', hessian=TRUE, silent=TRUE)
-    out <- NULL
-    opt <- try(
-        nlminb(obj$par, obj$fn, obj$gr), silent=TRUE
-    )
-    if (class(opt) != 'try-error') out <- opt
-    
-    return(out)
-}
-
-dllOut <- function(optObj, comp2)
-{
-    if (length(optObj$par) != length(comp2))
-        stop("# of parameters don't match")
-    
-    out <- matrix(
-        NA,
-        nrow=2,
-        ncol=length(comp2)+1,
-        dimnames=list(c(1, 'true'), c(names(comp2), 'conv'))
-    )
-    out[1, ] <- c(optObj$par, optObj$convergence)
-    out[2, ] <- c(comp2, NaN)
-    
-    return(out)
 }
---
+++ THESIS/code/testv4_initial_guess.R
@@ -0,0 +1,114 @@
+##----------------------------------------------------------------------
+##                                                     Henrique Laureano
+##                                            henriquelaureano.github.io
+##                                      2021-fev-23 · Curitiba/PR/Brazil
+##----------------------------------------------------------------------
+
+## ~/Git/henriquelaureano.github.io/THESIS/code/
+source('functions.R')
+
+## install.packages('pacman')
+pacman::p_load(Matrix, mvtnorm, mc2d, ## rmultinomial()
+               furrr,                 ## future + purrr
+               TMB, tictoc)
+
+future::plan(multicore);TMB::openmp(11)
+
+## TAKE-HOME MESSAGE ---------------------------------------------------
+## DO ALL INITIAL GUESSES LEAD TO THE SAME PLACE?
+
+## TESTING WITH THE BIGGER (MOST COMPLICATED?) MODEL IN THE MOST
+## DIFFICULT SCENARIO (SMALLER GROUPS AND HIGHEST CENSORSHIP)
+
+beta  <- c(-2, -1.5)
+gama  <- c(1.2, 1)
+w     <- c(3, 5)
+s2_1  <- 1.0
+s2_2  <- 0.6
+s2_3  <- 0.7
+s2_4  <- 0.9
+rho12 <-  0.1
+rho13 <- -0.5
+rho14 <-  0.3
+rho23 <-  0.3
+rho24 <- -0.4
+rho34 <-  0.2
+Sigma <- sigmaPD4(s2_1=s2_1,
+                  s2_2=s2_2,
+                  s2_3=s2_3,
+                  s2_4=s2_4,
+                  rho12=rho12,
+                  rho13=rho13,
+                  rho14=rho14,
+                  rho23=rho23,
+                  rho24=rho24,
+                  rho34=rho34)
+Sigma
+
+J     <- 1e4
+cs    <- 2
+time  <- runif(cs*J, 30, 79.9)
+delta <- 80
+Z     <- matrixZ(J=J, cs=cs)
+U     <- matrix(0, nrow=J, ncol=4)
+
+y <- datasimu(J=J, cs=cs, time=time,
+              beta=beta, gama=gama, w=w, latent='complete', Sigma=Sigma)
+prop.table(colSums(y))
+
+## ~/Git/henriquelaureano.github.io/THESIS/code/
+dll <- 'v4';invisible(TMB::compile(paste0('cpps/', dll, '.cpp')))
+## dyn.load(TMB::dynlib(dll))
+
+coefs <- data.frame(
+    beta1    =c(0, 0, -2.0, rep(NA, 3)),
+    beta2    =c(0, 0, -1.5, rep(NA, 3)),
+    gama1    =c(0, 0,  1.2, rep(NA, 3)),
+    gama2    =c(0, 0,  1.0, rep(NA, 3)),
+    w1       =c(1, 1,  3.0, rep(NA, 3)),
+    w2       =c(1, 1,  5.0, rep(NA, 3)),
+    logs2_1  =c(log(0.1), log(0.1), log(1.0), rep(NA, 3)),
+    logs2_2  =c(log(0.1), log(0.1), log(0.6), rep(NA, 3)),
+    logs2_3  =c(log(0.1), log(0.1), log(0.7), rep(NA, 3)),
+    logs2_4  =c(log(0.1), log(0.1), log(0.9), rep(NA, 3)),
+    rhoZ12   =c(atanh(0.05), atanh( 0.05), atanh( 0.1), rep(NA, 3)),
+    rhoZ13   =c(atanh(0.05), atanh(-0.05), atanh(-0.5), rep(NA, 3)),
+    rhoZ14   =c(atanh(0.05), atanh( 0.05), atanh( 0.3), rep(NA, 3)),
+    rhoZ23   =c(atanh(0.05), atanh( 0.05), atanh( 0.3), rep(NA, 3)),
+    rhoZ24   =c(atanh(0.05), atanh(-0.05), atanh(-0.4), rep(NA, 3)),
+    rhoZ34   =c(atanh(0.05), atanh( 0.05), atanh( 0.2), rep(NA, 3)),
+    conv     =rep(NA, 6), 
+    mll      =rep(NA, 6),
+    row.names=c(paste0('init', 1:3), paste0('fit', 1:3))
+)
+
+for (i in seq(3))
+{
+    checkDLL(dll)
+    obj <- TMB::MakeADFun(data=list(Y=y, Z=Z, time=time, delta=delta),
+                          parameters=list(beta1  =coefs[i, 'beta1'],
+                                          beta2  =coefs[i, 'beta2'],
+                                          gama1  =coefs[i, 'gama1'],
+                                          gama2  =coefs[i, 'gama2'],
+                                          w1     =coefs[i, 'w1'],
+                                          w2     =coefs[i, 'w2'],
+                                          logs2_1=coefs[i, 'logs2_1'],
+                                          logs2_2=coefs[i, 'logs2_2'],
+                                          logs2_3=coefs[i, 'logs2_3'],
+                                          logs2_4=coefs[i, 'logs2_4'],
+                                          rhoZ12 =coefs[i, 'rhoZ12'],
+                                          rhoZ13 =coefs[i, 'rhoZ13'],
+                                          rhoZ14 =coefs[i, 'rhoZ14'],
+                                          rhoZ23 =coefs[i, 'rhoZ23'],
+                                          rhoZ24 =coefs[i, 'rhoZ24'],
+                                          rhoZ34 =coefs[i, 'rhoZ34'], 
+                                          U=U),
+                          DLL=dll, random='U', hessian=TRUE, silent=TRUE)
+    tictoc::tic()
+    opt <- with(obj, nlminb(par, fn, gr))
+    tictoc::toc()
+    coefs[i+3, ] <- c(opt$par, opt$conv, opt$obj)
+    TMB::FreeADFun(obj);gc()
+}
+
+coefs
