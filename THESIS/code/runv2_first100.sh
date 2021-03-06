6e2114649539389630d028f1bd205d474e729538
Henrique Laureano <henriquelaureano@outlook.com>
2021-03-06 11:33:29 -0300

    daily backup

---
+++ THESIS/code/runv2_first100.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for i in {1..100}
+do
+  echo 'R CMD BATCH -$i v2_cs1_1.R'
+  R CMD BATCH -$i v2_cs1_1.R
+
+  echo 'R CMD BATCH -$i v2_cs1_2.R'
+  R CMD BATCH -$i v2_cs1_2.R
+
+  echo 'R CMD BATCH -$i v2_cs2_1.R'
+  R CMD BATCH -$i v2_cs2_1.R
+
+  echo 'R CMD BATCH -$i v2_cs2_2.R'
+  R CMD BATCH -$i v2_cs2_2.R
+
+  echo 'R CMD BATCH -$i v2_cs3_1.R'
+  R CMD BATCH -$i v2_cs3_1.R
+
+  echo 'R CMD BATCH -$i v2_cs3_2.R'
+  R CMD BATCH -$i v2_cs3_2.R
+done
--- THESIS/code/runv4.sh
+++ THESIS/code/runv4.sh
@@ -2,21 +2,39 @@
 
 for i in {1..100}
 do
-  echo 'R CMD BATCH -$i v4_cs1_1.R'
-  R CMD BATCH -$i v4_cs1_1.R
+  echo 'R CMD BATCH -$i v4_cs1_1_1.R'
+  R CMD BATCH -$i v4_cs1_1_1.R
 
-  echo 'R CMD BATCH -$i v4_cs1_2.R'
-  R CMD BATCH -$i v4_cs1_2.R
+  echo 'R CMD BATCH -$i v4_cs1_2_1.R'
+  R CMD BATCH -$i v4_cs1_2_1.R
 
-  echo 'R CMD BATCH -$i v4_cs2_1.R'
-  R CMD BATCH -$i v4_cs2_1.R
+  echo 'R CMD BATCH -$i v4_cs2_1_1.R'
+  R CMD BATCH -$i v4_cs2_1_1.R
 
-  echo 'R CMD BATCH -$i v4_cs2_2.R'
-  R CMD BATCH -$i v4_cs2_2.R
+  echo 'R CMD BATCH -$i v4_cs2_2_1.R'
+  R CMD BATCH -$i v4_cs2_2_1.R
 
-  echo 'R CMD BATCH -$i v4_cs3_1.R'
-  R CMD BATCH -$i v4_cs3_1.R
+  echo 'R CMD BATCH -$i v4_cs3_1_1.R'
+  R CMD BATCH -$i v4_cs3_1_1.R
 
-  echo 'R CMD BATCH -$i v4_cs3_2.R'
-  R CMD BATCH -$i v4_cs3_2.R
+  echo 'R CMD BATCH -$i v4_cs3_2_1.R'
+  R CMD BATCH -$i v4_cs3_2_1.R
+
+  echo 'R CMD BATCH -$i v4_cs1_1_2.R'
+  R CMD BATCH -$i v4_cs1_1_2.R
+
+  echo 'R CMD BATCH -$i v4_cs1_2_2.R'
+  R CMD BATCH -$i v4_cs1_2_2.R
+
+  echo 'R CMD BATCH -$i v4_cs2_1_2.R'
+  R CMD BATCH -$i v4_cs2_1_2.R
+
+  echo 'R CMD BATCH -$i v4_cs2_2_2.R'
+  R CMD BATCH -$i v4_cs2_2_2.R
+
+  echo 'R CMD BATCH -$i v4_cs3_1_2.R'
+  R CMD BATCH -$i v4_cs3_1_2.R
+
+  echo 'R CMD BATCH -$i v4_cs3_2_2.R'
+  R CMD BATCH -$i v4_cs3_2_2.R
 done
